fd53daa36cebe33d18193903c2226c0f3a14c5ab
Vidya A. Surendran <Vidya.Surendran@marlabs.com>
2018-07-06 15:48:34 +0530

    --amend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">Header
-        </header>
-        <p className="App-intro">
-          Testing
-        </p>
+        Cleaning complete
       </div>
     );
   }
